46d7ae8f544a87f7b57bd6fdbb183128af328202
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-01-02 17:46:58 +0100

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 class Solution {
 public:
-	int numPairsDivisibleBy60(vector<int>& time) {
 
-		const int TIME = 60;
-		vector<int> timeRemainder(TIME, 0);
-		int totalPairsDivisibleBy60 = 0;
+    int numPairsDivisibleBy60(vector<int>& time) {
 
-		for (const auto& duration : time) {
-			int checkIndex = (duration % TIME) != 0 ? (TIME - duration % TIME) : 0;
-			totalPairsDivisibleBy60 += timeRemainder[checkIndex];
-			timeRemainder[(duration % TIME)]++;
-		}
+        const int TIME = 60;
+        vector<int> timeRemainder(TIME, 0);
+        int totalPairsDivisibleBy60 = 0;
 
-		return totalPairsDivisibleBy60;
-	}
+        for (const auto& duration : time) {
+            int checkIndex = (duration % TIME) != 0 ? (TIME - duration % TIME) : 0;
+            totalPairsDivisibleBy60 += timeRemainder[checkIndex];
+            timeRemainder[(duration % TIME)]++;
+        }
+
+        return totalPairsDivisibleBy60;
+    }
 };
